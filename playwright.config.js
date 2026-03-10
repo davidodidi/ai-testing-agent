0cb08c0dac8f1e1366c4842d12d3e5dc58980722
davidodidi <davidodidi@yahoo.co.uk>
2026-03-09 23:20:10 -0400

    fix: remove hardcoded testDir so Playwright can run generated tests from any directory

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,6 @@
 const { defineConfig, devices } = require("@playwright/test");
 
 module.exports = defineConfig({
-  testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
